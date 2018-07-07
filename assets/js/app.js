6b65a2dc13e2b5ce5442215c544323a520c5220e
taylornok <taylorowenskees@outlook.com>
2018-07-07 11:46:53 -0700

    FP-working

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,20 @@
-$(document).ready(function() {
+$(document).ready(function () {
+
+    $.ajax({
+        url: "https://api-us.faceplusplus.com/facepp/v3/detect",
+        method: "POST",
+        data: {
+            api_key: "LIaHwfw7KLomS1KKtUiyXVsnrQih_Y4i",
+            api_secret: "c_qU60OC3uuVOhbZrNukuSmlTSohv1Ji",
+            return_attributes: "age",
+            image_url: "http://www.gstatic.com/tv/thumb/persons/1366/1366_v9_bb.jpg",
+
+        }
+    }).then(function (response) {
+        console.log(response);
+
+
+    });
 
 
 
@@ -7,5 +23,9 @@ $(document).ready(function() {
 
 
 
-    
+
+
+
+
+
 });
